2e9561fcf9845ac9cc31482fabc6bc4ecc8e10f8
Jatin Bhuva <bhuvajatin252@gmail.com>
2026-03-14 16:22:52 +0530

    implemented app icon and splashcreen for android

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import BootSplash from 'react-native-bootsplash';
 
 import { RootTabNavigator } from './navigation';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
+      <NavigationContainer onReady={() => BootSplash.hide({ fade: true })}>
         <RootTabNavigator />
       </NavigationContainer>
     </SafeAreaProvider>
